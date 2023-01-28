acb3225d0ac0c240df4c4d1ce4e63e0c78d4efc5
Edgar Cure <edgardavid1503@gmail.com>
2023-01-28 09:47:06 -0800

    Home page template finished

---
+++ chamber/scripts/app.js
@@ -0,0 +1,21 @@
+const datefield = document.querySelector(".date");
+
+
+const now = new Date();
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(now);
+
+
+datefield.innerHTML = `<p>${fulldate}</p>`;
+
+const year = now.getFullYear();
+
+document.getElementById("webInfo").innerHTML = `&copy; ${year} | Edgar Cure | WDD 230 Project | Last Modified on: ${document.lastModified}`
+
+function toggleMenu(){
+    document.getElementById("primaryNav").classList.toggle("open");
+    document.getElementById("hamburguerBtn").classList.toggle("open");
+
+}
+
+const x = document.getElementById("hamburguerBtn");
+x.onclick = toggleMenu;
