49971f6f645353dadeae86789cd4b4fab1a337aa
Edgar Cure <edgardavid1503@gmail.com>
2023-02-02 11:49:02 -0800

    content added and completed with base css

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -19,3 +19,30 @@ function toggleMenu(){
 
 const x = document.getElementById("hamburguerBtn");
 x.onclick = toggleMenu;
+
+
+
+function weatherBalloon( cityID ) {
+    var key = "582b55efa5e27910234adbbf3225cdad";
+    fetch("https://api.openweathermap.org/data/2.5/weather?id=" + cityID+ "&appid=" + "3045dd712ffe6e702e3245525ac7fa38")  
+    .then(function(resp) { return resp.json() }) // Convert data to json
+    .then(function(data) {
+      drawWeather(data);
+    })
+    .catch(function() {
+      // catch any errors
+    });
+  }
+  
+  window.onload = function() {
+    weatherBalloon( 5374232 );
+}
+
+function drawWeather( d ) {
+    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	//document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
