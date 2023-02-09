8b4a26bae486f4d300fa98970608fcb1585ae508
Edgar Cure <edgardavid1503@gmail.com>
2023-02-08 19:55:08 -0800

    corrected wave error

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -45,15 +45,28 @@ function weatherBalloon( cityID ) {
   }
   
   window.onload = function() {
-    weatherBalloon( 5374232 );
+ 
+  weatherBalloon( 5374232 );
 }
 
 function drawWeather( d ) {
-    var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+  var celcius = Math.round(parseFloat(d.main.temp)-273.15);
 	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
 	
 	//document.getElementById('description').innerHTML = d.weather[0].description;
 	document.getElementById('temp').innerHTML = fahrenheit + '&deg;';
 	document.getElementById('location').innerHTML = d.name;
+  const windSpeed = d.wind.speed;
+  const mph = parseFloat(windSpeed * 2.236936).toFixed(1);
+  document.getElementById('windSpeed').innerHTML = `Wind Speed: ${mph} mph`
+  if (fahrenheit <= 50 && mph > 3.0){
+    const windChillVar = parseFloat(35.74 + (0.6215 * fahrenheit) - (35.75 * (mph ** 0.16)) + (0.4275 * fahrenheit * (mph ** 0.16))).toFixed(1);
+    document.getElementById("windChill").innerHTML = `Wind Chill: ${windChillVar}&deg;`
+  } 
+  else {
+    const windChillVar = "N/A";
+    document.getElementById("windChill").innerHTML = `Wind Chill: ${windChillVar}`
+    
+  }
+  
 }
-
