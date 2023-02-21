bc5151f611335108ca2fb2dc5943a6491d583909
Edgar Cure <edgardavid1503@gmail.com>
2023-02-21 11:26:27 -0800

    finished assignment week 7

--- chamber/scripts/app.js
+++ chamber/scripts/app.js
@@ -70,3 +70,34 @@ function drawWeather( d ) {
   }
   
 }
+
+let imagesToLoad = document.querySelectorAll("img[data-src]");
+const loadImages = (image) => {
+  image.setAttribute("src", image.getAttribute("data-src"));
+  image.onload = () => {
+    image.removeAttribute("data-src");
+  };
+};
+
+let options = {
+    root: document.querySelector("#scrollArea"),
+    rootMargin: "0px 0px 50px 0px",
+    threshold: 1
+  }
+  
+
+if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);}
+        }, options);
+      }); imagesToLoad.forEach((img) => {
+      observer.observe(img);
+    });
+  } else {
+    imagesToLoad.forEach(img => {
+      loadImages(img);
+    });
+  }
