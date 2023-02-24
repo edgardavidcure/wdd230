83e5627c55e69bbeb1a96685b3e7ee3dc500f342
Edgar Cure <edgardavid1503@gmail.com>
2023-02-24 08:33:11 -0800

    added thankyou page

---
+++ chamber/scripts/joindate.js
@@ -0,0 +1,6 @@
+const d = new Date();
+const currentDate = d.toDateString();
+const hour = d.getHours();
+const mins = d.getMinutes();
+const seconds = d.getSeconds();
+document.getElementById("date").value = `${currentDate}, ${hour}:${mins}:${seconds}`
