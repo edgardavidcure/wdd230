f080b8e0ab1153b0eb013b275664a3a9a882c24e
Edgar Cure <edgardavid1503@gmail.com>
2023-04-03 14:42:30 -0700

    added functionality and modal to display order summary

--- final/scripts/fruits.js
+++ final/scripts/fruits.js
@@ -1,7 +1,90 @@
+const orderName = document.getElementById("order-name");
+const orderEmail = document.getElementById("order-email");
+const orderPhone = document.getElementById("order-phone");
+const orderFruits = document.getElementById("order-fruits");
+const orderInstructions = document.getElementById("order-instructions");
+const orderDate = document.getElementById("order-date");
+const carbs = document.getElementById("carbohydrates");
+const protein = document.getElementById("protein");
+const fat = document.getElementById("fat")
+const sugar = document.getElementById("sugar");
+const calories = document.getElementById("calories");
+
+
+const inputName = document.getElementById("fname");
+const inputEmail = document.getElementById("email");
+const inputPhone = document.getElementById("phone");
+const inputFruit1 = document.getElementById("fruit1");
+const inputFruit2 = document.getElementById("fruit2");
+const inputFruit3 = document.getElementById("fruit3");
+const instructions = document.getElementById("instructions");
+const dateField = document.getElementById("date");
+const date = new Date()
+const fulldate = new Intl.DateTimeFormat("en-US", { dateStyle: "full" }).format(
+    date
+  );
+
+let selectedFruits = [];
+let caloriesTotal = 0;
+let carbsTotal = 0;
+let proteinTotal = 0;
+let fatTotal = 0;
+let sugarTotal = 0; 
+let fruit1Value;
+let fruit2Value;
+let fruit3value;
+
+
+
+
+function nutritionLabel(fruitsData){
+    form.addEventListener("submit", function() {
+        const nameValue = inputName.value;
+        orderName.innerText = nameValue;
+        const emailValue = inputEmail.value;
+        orderEmail.innerText = emailValue;
+        const phoneValue = inputPhone.value;
+        orderPhone.innerText = phoneValue;
+        fruit1Value = inputFruit1.value;
+        fruit2Value = inputFruit2.value;
+        fruit3value = inputFruit3.value;
+        orderFruits.innerText = `${fruit1Value}, ${fruit2Value}, ${fruit3value}`;
+        const instructionsValue = instructions.value;
+        if (!instructionsValue){
+            orderInstructions.innerText = "None"
+        } else {
+            orderInstructions.innerText = instructionsValue
+        }
+        orderDate.innerText = fulldate
+        selectedFruits = fruitsData.filter(
+            (fruit) => fruit.name === fruit1Value || fruit.name === fruit2Value || fruit.name === fruit3value
+        );
+        selectedFruits.forEach(fruit => {
+            caloriesTotal += fruit.nutritions.calories
+            carbsTotal += fruit.nutritions.carbohydrates
+            fatTotal += fruit.nutritions.fat
+            proteinTotal += fruit.nutritions.protein
+            sugarTotal += fruit.nutritions.sugar
+        })
+        carbs.innerText = `${Math.round(carbsTotal)}g`
+        protein.innerText = `${Math.round(proteinTotal)}g`
+        fat.innerText = `${Math.round(fatTotal)}g`
+        sugar.innerText = `${Math.round(sugarTotal)}g`
+        calories.innerText = `${Math.round(caloriesTotal)}g`
+        console.log(carbsTotal)
+        console.log(selectedFruits)
+
+    })
+    
+}
+
+
+
 async function getFruitsData(){
     const response = await fetch("json/fruits.json");
     const data = await response.json();
     appendFruitOptions(data.fruits)
+    nutritionLabel(data.fruits)
 }
 
 const appendFruitOptions = (fruits) => {
@@ -17,3 +100,5 @@ const appendFruitOptions = (fruits) => {
 }
 
 getFruitsData()
+
+
