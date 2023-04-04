2932fcab28005d62f007e7064db09f7f0bcd6b4b
Edgar Cure <edgardavid1503@gmail.com>
2023-04-04 09:45:54 -0700

    Added functionality to home info card

--- final/scripts/homelocalstorage.js
+++ final/scripts/homelocalstorage.js
@@ -1,9 +1,14 @@
 let submissionCount = localStorage.getItem("submission_count");
-
+const text = document.getElementById("card-message");
 if (!submissionCount) {
   submissionCount = 0;
+  text.innerText = "NO DRINKS YET?! LET US TAKE CARE OF YOU"
+} 
+if(submissionCount === 1){
+  text.innerText = "I BET YOU LIKED THAT FIRST DRINK, LETS GET YOU ANOTHER ONE"
+} else{
+  text.innerText = "A FEW DRINKS ALREADY, HUH? LETS GET YOU ANOTHER ONE"
 }
 localStorage.setItem("submission_count", submissionCount);
 
 document.querySelector("#submission-count").textContent = submissionCount;
-
